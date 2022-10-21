c07fd54fb89ba57e9d14d875d74f30aab48aa4d4
pjhhs021 <pjhhs021@gmail.com>
2022-10-21 17:20:24 +0900

    new

---
+++ src/main/java/com/yjh/rememberme/RememberMeApplication.java
@@ -0,0 +1,13 @@
+package com.yjh.rememberme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RememberMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yjh/rememberme/RememberMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh.rememberme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
