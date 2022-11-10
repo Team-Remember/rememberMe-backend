f9610ecb9fc9053e038b45eef3ce09831f20144f
yeonjae <holmanyj@gmail.com>
2022-11-10 18:24:30 +0900

    AdminChatBotController 추가

---
+++ src/main/java/com/yjh/rememberme/database/repository/ChatBotRepository.java
@@ -0,0 +1,10 @@
+package com.yjh.rememberme.database.repository;
+
+import com.yjh.rememberme.database.entity.ChatBot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ChatBotRepository extends JpaRepository<ChatBot, Integer> {
+
+    int countAllByChatBotIdIsNotNull();
+}
