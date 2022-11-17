e7eb320c450edc690ac198ed753768d98b5a8e56
pjhhs021 <pjhhs021@gmail.com>
2022-11-17 16:18:53 +0900

    fix: add Transactional

--- src/main/java/com/yjh/rememberme/auth/service/MemberDetailsService.java
+++ src/main/java/com/yjh/rememberme/auth/service/MemberDetailsService.java
@@ -4,7 +4,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import javax.transaction.Transactional;
+
 public interface MemberDetailsService extends UserDetailsService {
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
