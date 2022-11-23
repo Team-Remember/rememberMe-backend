4d784dd4147282a88699439760062800995828ea
pjhhs021 <pjhhs021@gmail.com>
2022-11-23 17:48:08 +0900

    #61 feature : add room api

--- src/main/java/com/yjh/rememberme/database/repository/RoomRepository.java
+++ src/main/java/com/yjh/rememberme/database/repository/RoomRepository.java
@@ -4,4 +4,10 @@ import com.yjh.rememberme.database.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+    Room findByRoomName(String roomname);
+
+    Room findByRoomNameAndMemberId(String roomname, Integer id);
+
+    Room findById(int roomid);
+
 }
--- src/main/java/com/yjh/rememberme/room/service/RoomService.java
+++ src/main/java/com/yjh/rememberme/room/service/RoomService.java
@@ -1,7 +1,51 @@
 package com.yjh.rememberme.room.service;
 
+import com.yjh.rememberme.database.entity.Room;
+import com.yjh.rememberme.database.repository.MemberRepository;
+import com.yjh.rememberme.database.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class RoomService {
+    private final RoomRepository roomRepository;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public RoomService(RoomRepository roomRepository, MemberRepository memberRepository) {
+        this.roomRepository = roomRepository;
+        this.memberRepository = memberRepository;
+    }
+    @Transactional
+    public int createRoom(String username, String roomname) {
+        try{ roomRepository.save(new Room(
+                0,
+                roomname,
+                Room.RoomStatus.PUBLIC,
+                0,
+                0,
+                memberRepository.findByNickname(username).getId()
+        ));
+        }catch (Exception e) {
+            System.out.println(e);
+            System.out.println("postRoom fail");
+        }
+        return roomRepository.findByRoomName(roomname).getId();
+    }
+    @Transactional
+    public Room.RoomStatus patchRoomPrivate(String username, String roomname) {
+        Room roomStatus2 = roomRepository.findByRoomNameAndMemberId(roomname, memberRepository.findByUsername(username).getId());
+        roomStatus2.setRoomStatus(Room.RoomStatus.PRIVATE);
+        roomRepository.save(roomStatus2);
+        return roomStatus2.getRoomStatus();
+    }
+    @Transactional
+    public Room.RoomStatus patchRoomPublic(String username, String roomname) {
+        Room roomStatus2 = roomRepository.findByRoomNameAndMemberId(roomname, memberRepository.findByUsername(username).getId());
+        roomStatus2.setRoomStatus(Room.RoomStatus.PUBLIC);
+        roomRepository.save(roomStatus2);
+        return roomStatus2.getRoomStatus();
+    }
 }
