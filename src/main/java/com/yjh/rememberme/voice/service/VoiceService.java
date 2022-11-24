572020abc381511f8a9b693b6641f81fbb3b04ee
yeonjae <holmanyj@gmail.com>
2022-11-24 16:43:03 +0900

    voice controller, service 추가

--- src/main/java/com/yjh/rememberme/voice/service/VoiceService.java
+++ src/main/java/com/yjh/rememberme/voice/service/VoiceService.java
@@ -8,17 +8,20 @@ import com.yjh.rememberme.database.repository.VoiceLogRepository;
 import com.yjh.rememberme.database.repository.VoiceRepository;
 import com.yjh.rememberme.voice.dto.VoiceDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.transaction.Transactional;
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.nio.charset.Charset;
+import java.util.*;
 
 @Service
 public class VoiceService {
@@ -70,8 +73,8 @@ public class VoiceService {
     public MultiValueMap<String, Object> addBody(VoiceDTO voiceDTO) {
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         body.add("voice", voiceDTO.getVoice().getResource());
-        body.add("userId", memberRepository.findByUsername(voiceDTO.getUserNickname()).getId());
-        body.add("weId", memberRepository.findByUsername(voiceDTO.getOpponentNickname()).getId());
+        body.add("userId", memberRepository.findByNickname(voiceDTO.getUserNickname()).getId());
+        body.add("weId", memberRepository.findByNickname(voiceDTO.getOpponentNickname()).getId());
 
         return body;
     }
@@ -85,5 +88,67 @@ public class VoiceService {
         map.put("voicePathList",voiceRepository.findAllByMemberIdAndOpponentId(userId,weId));
 
         return map;
+    }
+
+    public ResponseEntity<?> callSTT(VoiceDTO voiceDTO, int userId, int weId) {
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+        headers2.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+
+        body = addBody(voiceDTO);
+
+        HttpEntity<?> requestEntity = new HttpEntity<>(body, headers2);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "ai stt 서버 url";
+        System.out.println("body = " + body);
+
+        ResponseEntity<?> resultMap = restTemplate.postForEntity(url, requestEntity, byte[].class);
+
+        return resultMap;
+    }
+
+    public ResponseEntity<?> callChatBot(ResponseEntity<?> resultMap, int userId, int weId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("data", resultMap);
+        body.put("userId", userId);
+        body.put("weId", weId);
+
+        HttpEntity<?> requestEntity = new HttpEntity<>(body, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://9e5a-119-194-163-123.jp.ngrok.io/chat_bot";
+
+        ResponseEntity<?> resultMap2 = restTemplate.postForEntity(url, requestEntity, Map.class);
+
+        return resultMap2;
+    }
+
+    public ResponseEntity<?> callTTS(ResponseEntity<?> resultMap2, int userId, int weId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("data", resultMap2);
+        body.put("userId", userId);
+        body.put("weId", weId);
+
+        HttpEntity<?> requestEntity = new HttpEntity<>(body, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://9e5a-119-194-163-123.jp.ngrok.io/chat_bot";
+
+        ResponseEntity<?> resultMap3 = restTemplate.postForEntity(url, requestEntity, byte[].class);
+
+        return resultMap3;
     }
 }
